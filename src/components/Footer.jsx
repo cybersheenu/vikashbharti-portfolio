9c5ac53e730242d5deb7fdc0e58303e00554ef34
Vikash Bharti <cybersheenu@gmail.com>
2025-12-24 02:43:26 +0530

    fix: remove unused vars from Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,9 @@
 import React from "react";
-import { portfolioData } from "../data/portfolioData";
 
 function Footer() {
-  const { instagram, linkedin, youtube, github, tryhackme } =
-    portfolioData.socialLinks;
-
   return (
     <footer className="footer">
-      {/* <a href={instagram} target="_blank" rel="noreferrer">
-        Instagram
-      </a>
-      <a href={linkedin} target="_blank" rel="noreferrer">
-        LinkedIn
-      </a>
-      <a href={youtube} target="_blank" rel="noreferrer">
-        YouTube
-      </a>
-      <a href={github} target="_blank" rel="noreferrer">
-        GitHub
-      </a>
-      <a href={tryhackme} target="_blank" rel="noreferrer">
-        TryHackMe
-      </a> */}
+      <p>© {new Date().getFullYear()} CyberSheenu | Vikash Bharti</p>
     </footer>
   );
 }
